1461cb9de97b02dfbede39494f00a3dd010503af
DiegoMuniz92 <diegooliveiramuniz2014@gmail.com>
2023-10-30 19:02:40 -0400

    resolvendo o desafio contador 30-10

--- DESAFIOS/src/DesafioControleDeFluxo.java
+++ DESAFIOS/src/DesafioControleDeFluxo.java
@@ -2,24 +2,32 @@ import java.util.Scanner;
 
 public class DesafioControleDeFluxo {
     public static void main(String[] args) {
-        try (Scanner terminal = new Scanner(System.in)) {
-            int parametroUm = terminal.nextInt();
-            int parametroDois = terminal.nextInt();
-            int contador = 0;
+        
+        Scanner terminal = new Scanner(System.in);
+        int parametroUm = terminal.nextInt();
+        int parametroDois = terminal.nextInt();
+        System.out.println(" ");
 
-            if (parametroUm < parametroDois) {
-                int contar = parametroDois - parametroUm;
-                System.out.println(contar);
-                while (contador < contar) {
-                    System.out.println(contador+1);
-                    contador++;
-                }
-            }
+        try {
+            conta(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosExceptions exceptions) {
+            System.out.println("Informe o segundo valor maior que o primeiro.");
         }
+    }    
 
-       /* try {
-                //Chamando o método contendo a lógica de contagem
-                contar (parametroUm, parametroDois);
-        } catch */
+    static void conta (int parametroUm, int parametroDois) throws ParametrosInvalidosExceptions {
+        int contar = parametroDois - parametroUm;
+        int contador = 0;
+
+        if (parametroDois > parametroUm) {
+           while (contador < contar) {
+            System.out.println(contador+1);
+            contador++;
+            } 
+        } else {
+            throw new ParametrosInvalidosExceptions();
+        }
+        
+        
     }
 }
