fac1010088524865b109a0649bd18a9db255018b
DiegoMuniz92 <diegooliveiramuniz2014@gmail.com>
2023-10-29 22:35:10 -0400

    desafio contador 29-10

--- DESAFIOS/src/ProcessoSeletivo.java
+++ DESAFIOS/src/ProcessoSeletivo.java
@@ -1,11 +1,62 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        selecaoCandidatos();
+        String [] candidatosSelecionados = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO"};
+		//primeiro um for para selecionar os candidatos
+		for(String candidato: candidatosSelecionados) {
+			entrandoEmContato(candidato);}
     }
+
+    static void entrandoEmContato(String candidato) {
+		
+		int tentativasRealizadas = 1;
+		boolean continuarTentando = true;
+		boolean atendeu=false;
+		do {
+			atendeu= atender();
+			continuarTentando = !atendeu;
+			if(continuarTentando)
+				tentativasRealizadas++;
+			else
+				System.out.println("CONTATO REALIZADO COM SUCESSO");
+			
+		}while(continuarTentando && tentativasRealizadas<3);
+		
+		if(atendeu)
+			System.out.println("CONSEGUIMOS CONTATO COM " + candidato +" NA " + tentativasRealizadas + " TENTATIVA");
+		else
+			System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato +", NÚMERO MAXIMO TENTATIVAS " + tentativasRealizadas + " REALIZADA");
+		
+		
+	}
+	
+	//método auxiliar
+	static boolean atender() {
+		return new Random().nextInt(3)==1;	
+	}
     
     //UTILIZANDO METODOS COM PASSAGEM DE PARAMETRO
+    static void imprimirSelecionados () {
+        String [] candidatos = {"Diego", "Cintia","Arthur", "Ricardo", "Miguel"};
+        
+        //forma indexada
+		//quando preciso do indice para complementar a lógica
+		System.out.println("Imprimindo com a ordem de seleção pelo índice");
+		for(int x=0; x<candidatos.length; x++) {
+			System.out.println((x+1)+ "° Candidato é " + candidatos [x] );
+		}
+
+        //forma abrevida
+		//interação total sem precisar da posição ou indice
+		System.out.println("Imprimindo todos sem a necessidade de exibir o índice");
+		
+		for(String candidato: candidatos) {
+			System.out.println(candidato);
+		}
+    }
+
     static void selecaoCandidatos (){
         String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
         int candidatosSelecionados = 0;
