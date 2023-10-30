0f90afff4a80f6ca8d3058471af79b703273e54e
DiegoMuniz92 <diegooliveiramuniz2014@gmail.com>
2023-10-30 17:53:10 -0400

    resolvendo desafio contador

---
+++ DESAFIOS/src/DesafioControleDeFluxo.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class DesafioControleDeFluxo {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+
+        int parametroUm = terminal.nextInt();
+        int parametroDois = terminal.nextInt();
+        int contador = 0;
+
+        if (parametroUm < parametroDois) {
+            int contar = parametroDois - parametroUm;
+            System.out.println(contar);
+            while (contador < contar) {
+                System.out.println(contador+1);
+                contador++;
+            }
+        }
+
+       /* try {
+                //Chamando o método contendo a lógica de contagem
+                contar (parametroUm, parametroDois);
+        } catch */
+    }
+}
